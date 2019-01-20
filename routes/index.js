95735c6ea851dd2014af70e6b539024ef5969e7e
Shane Chambry <Shaneechambry@gmail.com>
2019-01-19 22:09:08 -0700

    refactored sendmessage

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-console.log(process.env.SLACK_TOKEN);
 /* GET home page. */
 router.post('/', function (req, res, next) {
   console.log(req.headers);
@@ -11,37 +10,42 @@ router.post('/', function (req, res, next) {
   res.status(200).send({ "challenge": req.body.challenge });
 });
 
+function sendMessage(channel, text, cb) {
+  let body = JSON.stringify({
+    'channel': channel,
+    'text': text,
+    'as_user': 'false',
+    'username': 'Trilogy_Timer'
+  });
+
+  var options = {
+    url: 'https://slack.com/api/chat.postMessage',
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.SLACK_TOKEN}`
+    },
+    body: body
+  };
+
+  request.post(options, cb(error, response, body));
+}
+
+
+
 router.post('/timer', function (req, res, next) {
-  console.log(process.env.SLACK_TOKEN);
   console.log(req.headers);
   console.log(req.body);
   res.status(200).send({ "text": "Timer Started." });
 
+  sendMessage(req.body.channel, "Time Remaining", function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  })
 
   let test = setTimeout(function () {
     console.log('the timeout is happening');
-    let body = JSON.stringify({
-      'channel': 'CFJAX6E9L',
-      'text': 'message after timeout',
-      'as_user': 'false',
-      'username': 'Trilogy_Timer'
-    });
-
-    var options = {
-      url: 'https://slack.com/api/chat.postMessage',
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.SLACK_TOKEN}`
-      },
-      body: body
-    };
-
-    request.post(options, function (error, response, body) {
-      console.log('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', body); // Print the HTML for the Google homepage.
-    });
   }, 3000);
 
   // setTimeout(() => {
