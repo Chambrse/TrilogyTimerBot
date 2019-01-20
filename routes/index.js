a811ab7f23362e3ba7699f6e647dda1889bbbc6f
Shane Chambry <Shaneechambry@gmail.com>
2019-01-19 20:30:08 -0700

    added timer route and root confirmation response

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).send({ "text": "got it" });
+});
+
+router.post('/timer', function(req, res, next) {
+  res.status(200).send({ "text": "Timer Started" });
 });
 
 module.exports = router;
