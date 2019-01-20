205a38d78710dfc0fe6aa76c8afe6fdebc9e3ee2
Shane Chambry <Shaneechambry@gmail.com>
2019-01-19 20:43:53 -0700

    testing timeout functionality on heroku fingers crossed

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  console.log(req);
+  console.log(req.headers);
+  console.log(req.body);
+
   res.status(200).send({ "challenge": req.body.challenge });
 });
 
 router.post('/timer', function(req, res, next) {
-  res.status(200).send({ "text": "Timer Started" });
+  console.log(req.headers);
+  console.log(req.body);
+  setTimeout(() => {
+    
+    res.status(200).send({ "text": "in timeout function!" });
+  }, 1000);
 });
 
 module.exports = router;
