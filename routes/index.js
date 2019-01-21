14ae8471243805128198368d4baf1ee8a4b689e0
Shane Chambry <Shaneechambry@gmail.com>
2019-01-20 18:22:39 -0700

    responding to an interactive message

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.post('/iGetIt', function (req, res, next) {
   console.log(req.headers);
   console.log(req.body);
 
-  
+
   let body = JSON.stringify({
     'channel': req.body.channel_id,
     'as_user': 'false',
@@ -45,12 +45,12 @@ router.post('/iGetIt', function (req, res, next) {
       }
     ]
   });
-  
+
   slack.sendMessageNew(body, function (error, response, body) {
     console.log('body:', body); // Print the HTML for the Google homepage.
-    
+
     console.log(JSON.parse(body, null, 2).ts);
-    
+
   });
   res.status(200).send();
 
@@ -60,7 +60,36 @@ router.post('/actions', function (req, res, next) {
   console.log(req.headers);
   console.log(req.body);
 
-  res.status(200).send({ "challenge": req.body.challenge });
+  res.status(200).send({
+    'channel': req.body.channel_id,
+    'as_user': 'false',
+    'username': 'Trilogy_Timer',
+    "text": "Please rate your comprehension of this topic.",
+    "attachments": [{
+      "text": "someone responded to thise message."
+    },
+    {
+      "fallback": "Something Happened.",
+      "callback_id": "ComprehensionRating",
+      "color": "#3AA3E3",
+      "attachment_type": "default",
+      "actions": [
+        {
+          "name": "I Get It",
+          "text": "I get it!",
+          "type": "button",
+          "value": "iGetIt"
+        },
+        {
+          "name": "I Don't Get It",
+          "text": "What?",
+          "type": "button",
+          "value": "iDontGetIt"
+        }
+      ]
+    }
+    ]
+  });
 });
 
 router.post('/timer', function (req, res, next) {
