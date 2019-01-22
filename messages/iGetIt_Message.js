32e0668071be7bd4c353203f68bc5b1222a57caf
Shane Chambry <Shaneechambry@gmail.com>
2019-01-22 15:32:06 -0700

    removed json.parse from igetit_message

--- messages/iGetIt_Message.js
+++ messages/iGetIt_Message.js
@@ -1,5 +1,5 @@
 let iGetIt_Message = function (channel, responses) {
-    return JSON.stringify({
+    return {
         'channel': channel,
         'as_user': 'false',
         'username': 'Trilogy',
@@ -26,7 +26,7 @@ let iGetIt_Message = function (channel, responses) {
                 ]
             }
         ]
-    });
+    };
 };
 
 module.exports = iGetIt_Message;
